d6157e4815f813654e2e7f22ea922efc8214bfdc
ryanzola <ryanzola@me.com>
2023-04-22 12:08:40 -0400

    🧆 Boilerplate
    - add voxel.c boilerplate
    - add dos-like framework files
    - add dos-like lib files

--- voxel.c
+++ voxel.c
@@ -1,3 +1,33 @@
-int main(int argc, char* args[]){
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <math.h>
+#include "dos.h"
+
+#define SCREEN_WIDTH 320
+#define SCREEN_HEIGHT 200
+
+int main(int argc, char* args[]) {
+  setvideomode(videomode_320x200);
+
+  setdoublebuffer(1);
+  uint8_t* framebuffer = screenbuffer();
+
+  while(!shuttingdown()) {
+    waitvbl();
+    clearscreen();
+
+    // TODO:
+    // - processinput()
+    // - update()
+    // - render()
+
+    framebuffer = swapbuffers();
+
+    if(keystate(KEY_ESCAPE)) {
+      break;
+    }
+  }
+
   return 0;
 }
